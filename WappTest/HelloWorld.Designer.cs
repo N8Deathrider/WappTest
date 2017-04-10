240e6fad1d6b8af80ce425803b85371098213d34
Chris Hansen <tchansen@accela.com>
2017-04-10 13:14:40 -0600

    Added image button and dialog
    Added button
    Added dialog
    Added code to load dialog result into picture control
    Changed form to auto size

--- WappTest/HelloWorld.Designer.cs
+++ WappTest/HelloWorld.Designer.cs
@@ -30,11 +30,15 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pic_showMe = new System.Windows.Forms.PictureBox();
+            this.btn_showMe = new System.Windows.Forms.Button();
+            this.openFileDialog_showMe = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_showMe)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(104, 71);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -44,20 +48,42 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(44, 136);
+            this.textBox1.Location = new System.Drawing.Point(93, 14);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(164, 20);
             this.textBox1.TabIndex = 1;
+            // 
+            // pic_showMe
+            // 
+            this.pic_showMe.Location = new System.Drawing.Point(13, 82);
+            this.pic_showMe.Name = "pic_showMe";
+            this.pic_showMe.Size = new System.Drawing.Size(259, 167);
+            this.pic_showMe.TabIndex = 2;
+            this.pic_showMe.TabStop = false;
+            // 
+            // btn_showMe
+            // 
+            this.btn_showMe.Location = new System.Drawing.Point(13, 42);
+            this.btn_showMe.Name = "btn_showMe";
+            this.btn_showMe.Size = new System.Drawing.Size(75, 23);
+            this.btn_showMe.TabIndex = 3;
+            this.btn_showMe.Text = "Show Me";
+            this.btn_showMe.UseVisualStyleBackColor = true;
+            this.btn_showMe.Click += new System.EventHandler(this.btn_showMe_Click);
             // 
             // HelloWorld
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btn_showMe);
+            this.Controls.Add(this.pic_showMe);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "HelloWorld";
             this.Text = "MyWindow";
+            ((System.ComponentModel.ISupportInitialize)(this.pic_showMe)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -67,6 +93,9 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pic_showMe;
+        private System.Windows.Forms.Button btn_showMe;
+        private System.Windows.Forms.OpenFileDialog openFileDialog_showMe;
     }
 }
 
--- WappTest/HelloWorld.cs
+++ WappTest/HelloWorld.cs
@@ -22,5 +22,24 @@ namespace WappTest
         {
             textBox1.Text = "Yaeh";
         }
+
+        private void btn_showMe_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog_showMe.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                string file = openFileDialog_showMe.FileName;
+                Image myImage = Image.FromFile(file);
+                pic_showMe.Image = myImage;
+                pic_showMe.Height = myImage.Height;
+                pic_showMe.Width = myImage.Width;
+
+                int newWidth = (HelloWorld.ActiveForm.Width - pic_showMe.Width) + myImage.Width;
+                int newHeight = (HelloWorld.ActiveForm.Height - pic_showMe.Height) + myImage.Height;
+
+                this.Size = new Size(newWidth, newHeight);
+            }
+        }
     }
 }
